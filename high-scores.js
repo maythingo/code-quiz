0fa834753ceca412c2ff15c272d6109290069bde
maythingo <maythingo@aim.com>
2021-04-09 21:31:12 -0700

    added questions, and answers in high-scores.html

---
+++ high-scores.js
@@ -0,0 +1,7 @@
+var clear = document.querySelector("#clear");
+var back = document.querySelector("#back");
+
+//go back to beginning page
+back.addEventListener("click", function(){
+    window.location.replace("index.html")
+});
