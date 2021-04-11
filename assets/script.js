bb53ee9c9c0a2d6465e26a7a35c651f353a751cc
maythingo <maythingo@aim.com>
2021-04-10 18:44:46 -0700

    added timer

--- assets/script.js
+++ assets/script.js
@@ -28,4 +28,47 @@ var questions = [
 
 ];
 
+/*
+Timer:
+    - Initial timer is 75 seconds (D)
+    - Timer should start ticking when you press start quiz. (D)
+    - Format is just in seconds. 75 -> 74 -> 73 -> ... (D)
+    - Quiz is over when timer reaches 0 seconds.
+    - If you choose an incorrect answer, timer gets reduced by 10 seconds.
 
+Quiz:
+    - it should have a question in <p> tag.
+    - choices are buttons.
+    - when pressed, question can be correct or incorrect below all the answers.
+    - each question should go on until either time is up or user is done with all the questions.
+*/
+
+
+var timer = document.getElementById('time');
+var timerId;
+
+function startTimer() {
+    // setInterval that decrements by 1 until 0 is reached.
+    timerId = setInterval(function() {
+        // Get the time from index.html
+        var currentTime = parseInt(timer.textContent);
+        // Decrement the time.
+        currentTime = currentTime - 1;
+        timer.textContent = currentTime;
+        // if decemented time is remaining, keep on calling itself.
+        if (currentTime === 0) {
+            console.log('it is done!!');
+            clearInterval(timerId);
+        }
+        // else, stop the timer.
+    }, 1000);
+}
+
+
+
+
+var startButton = document.getElementById('start');
+  
+startButton.addEventListener("click",function() {
+   startTimer();
+});
--- high-scores.js
+++ high-scores.js
@@ -1,7 +1,27 @@
 var clear = document.querySelector("#clear");
 var back = document.querySelector("#back");
+var highScore = document.querySelector("#high-scores");
+
+
+
+//local storage
+var allScores = localStorage.getItem("allScores")
+allScores = JSON.parse(allScores);
+
+if (allScores !== null){
+    for (var i = 0; i < allScores.length; i++){
+        var liEl = document.createElement("li");
+        liEl.textContent = allScores[i].initials + "" + allScores[i].scores;
+    }
+}
+
+//clear scores
+clear.addEventListener("click",function(){
+    localStorage.clear();
+    location.reload();
+});
 
 //go back to beginning page
-back.addEventListener("click", function(){
-    window.location.replace("index.html")
+back.addEventListener("click", function () {
+    location.replace("index.html")
 });
